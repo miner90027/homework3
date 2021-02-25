05b2ce988159aa466b27d9e147c042a1840cb23a
miner90027 <alekszander.mccormick@outlook.com>
2021-02-25 10:38:58 -0900

    - created a struct to contain the information of a token - prototyped functions to convert lines to tokens, read lines, and to print the tokens

--- simpleTonkenizer.cpp
+++ simpleTonkenizer.cpp
@@ -1,6 +1,32 @@
+/* Aleks McCormick
+ * UAF CS 202
+ * Homework3
+ * Program 1: A Simple Tokenizer
+ * 02/25/2021
+ */
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::ostream;
+using std::istream;
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+struct TokenAndPosition{
+	string _token;
+	int _line;
+	unsigned int _column;
+};
+
+vector<string> lineToTokens(const string &line);
+vector<TokenAndPosition> readLines(istream &is);
+void printTokens(ostream &os, const vector<TokenAndPosition> &tokens);
+
+int main(int argc, char* argv[]) {
+
 	return 0;
 }
